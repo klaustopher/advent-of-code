184f09aabed21c6038419d688990a89fabce4e93
Klaus Zanders <code@kgz.me>
2019-12-01 12:53:45 +0100

    Start Implementation in C#

---
+++ 2019/AdventOfCode2019/AdventOfCode2019/Day11.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2019
+{
+    class Day11 : Challenge
+    {
+        private int calculateFuel(int mass)
+        {
+            return ((mass / 3) - 2);  
+        }
+        public string findResult()
+        {
+            List<int> items = Resources.Day1.Split(System.Environment.NewLine.ToCharArray()).
+                Where(x => !string.IsNullOrEmpty(x)).
+                Select(str => int.Parse(str)).
+                ToList();
+
+            int result = items.Select(mass => calculateFuel(mass)).Sum();
+
+            return result.ToString();
+        }
+    }
+}
