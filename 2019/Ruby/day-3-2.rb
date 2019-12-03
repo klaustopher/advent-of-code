b42b41a64c8a81f32d812a5bb21c552c6d244b8d
Klaus Zanders <klaus@kgz.me>
2019-12-03 08:12:08 +0100

    Day 3

---
+++ 2019/Ruby/day-3-2.rb
@@ -0,0 +1,40 @@
+Point = Struct.new(:x, :y, keyword_init: true) do
+    def distance
+      x.abs + y.abs
+    end
+  end
+  Direction = Struct.new(:direction, :steps, keyword_init: true)
+  
+  def parse_line(line_string)
+    line_string.split(',').map do |dir|
+      Direction.new(direction: dir[0].downcase.to_sym, steps: dir[1..-1].to_i)
+    end
+  end
+  
+  def build_points(line)
+    result = [Point.new(x: 0, y: 0)]
+  
+    line.each do |dir|
+      case dir.direction
+      when :r then dir.steps.times { prev_point = result.last; result << Point.new(x: prev_point.x + 1, y: prev_point.y) }
+      when :l then dir.steps.times { prev_point = result.last; result << Point.new(x: prev_point.x - 1, y: prev_point.y) }
+      when :u then dir.steps.times { prev_point = result.last; result << Point.new(x: prev_point.x, y: prev_point.y + 1) }
+      when :d then dir.steps.times { prev_point = result.last; result << Point.new(x: prev_point.x, y: prev_point.y - 1) }
+      end
+    end
+  
+    result
+  end
+  
+  lines = File.read('day3.txt').lines
+  
+  p1 = build_points(parse_line(lines[0]))
+  p2 = build_points(parse_line(lines[1]))
+  
+  intersections = p1 & p2 - [Point.new(x: 0, y: 0)]
+  
+  shortest = intersections.map do |inter|
+    p1.index(inter) + p2.index(inter)
+  end
+
+  puts shortest.min
