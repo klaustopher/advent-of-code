2c2e09553ff17aeffebc6935584cb8a8b460127c
Klaus Zanders <code@kgz.me>
2020-12-01 11:41:16 +0100

    Add 2020 - Day 1

---
+++ 2020/Ruby/day-1-1.rb
@@ -0,0 +1,11 @@
+require 'awesome_print'
+
+content = File.read('day-1.txt').lines.map(&:to_i)
+
+content.combination(2).each do |combination|
+  if combination.sum == 2020
+    puts combination.inject(:*)
+    break
+  end
+end
+
