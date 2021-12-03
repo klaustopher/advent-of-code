bb75991e8a6723e9a1c55fa89352c202fa964831
Klaus Zanders <code@kgz.me>
2021-12-03 09:07:43 +0100

    Day 3

---
+++ 2021/ruby/3-1.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+data = File.read('3-test.txt').lines.map(&:chomp)
+
+gamma_rate = ""
+epsilon_rate = ""
+
+number_of_bits = data.first.length
+
+number_of_bits.times do |i|
+  bits_at_position = data.map { |d| d[i] }.tally
+
+  if bits_at_position['1'] > bits_at_position['0']
+    gamma_rate += '1'
+    epsilon_rate += '0'
+  else
+    gamma_rate += '0'
+    epsilon_rate += '1'
+  end
+end
+
+puts gamma_rate.to_i(2) * epsilon_rate.to_i(2)
