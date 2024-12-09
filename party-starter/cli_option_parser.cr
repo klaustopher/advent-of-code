f3e62f1cb8c79b089596881015391e97fdd48738
Klaus Zanders <code@kgz.me>
2024-12-09 17:35:52 +0100

    allow overriding day in party starter

--- party-starter/cli_option_parser.cr
+++ party-starter/cli_option_parser.cr
@@ -1,5 +1,6 @@
 def parse_cli_options(available_languages, default_language)
   programming_language = default_language
+  puzzle_day = Time.local.day
 
   OptionParser.parse do |parser|
     parser.banner = "Advent of Code Daily Starter"
@@ -7,6 +8,16 @@ def parse_cli_options(available_languages, default_language)
     parser.on "-h", "--help", "Show help" do
       puts parser
       exit
+    end
+
+    parser.on "-d DAY", "--day DAY", "Day of the Advent of Code challenge" do |day|
+      if (1..25).includes? day.to_i
+        puzzle_day = day.to_i
+      else
+        puts "Unfortunately, #{day.colorize(:red)} is not a valid day."
+        puts "Valid days: 1-25"
+        exit(1)
+      end
     end
 
     parser.on "-l LANGUAGE", "--lang LANGUAGE", "Programming language that should be generated (default=#{default_language})" do |lang|
@@ -33,5 +44,5 @@ def parse_cli_options(available_languages, default_language)
     end
   end
 
-  return programming_language
+  return programming_language, puzzle_day
 end
